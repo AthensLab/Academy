cce0c027ae2c6cfc6c89c29962151b8c8f36bb8a
arlav <dounas@gmail.com>
2022-10-27 10:08:50 +0100

    Create Storage.sol

---
+++ Contracts/Storage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Storage {
+
+    uint256 public number; //this makes the uint readable by default,
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    /**
+     * @dev Return value
+     * @custom:you can comment the retrieve function out, if you have set the uint256 in public
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
